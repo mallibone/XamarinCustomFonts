9f06260c049ad5a616fb8bd49e805cfc84c9f419
Mark Allibone <mallibone@NoserMBP008.local>
2018-01-18 07:54:52 +0100

    Initial commit

---
+++ CustomFont/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PureLayout.Net;
+using UIKit;
+
+namespace CustomFont
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            var label = new UILabel
+            {
+                Text = "Hello",
+                Font = UIFont.FromName("GreatVibes-Regular", 42f),
+
+            };
+
+            View.BackgroundColor = UIColor.White;
+            View.AddSubview(label);
+
+            label.AutoPinEdgeToSuperviewEdge(ALEdge.Top, 32f);
+            label.AutoAlignAxisToSuperviewAxis(ALAxis.Vertical);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
